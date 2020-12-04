f96849f168152190842fc576893d5f5c4f59eaad
Guan Xian <gxleow@gmail.com>
2020-12-04 11:13:29 +0800

    EventsScreen

--- App.js
+++ App.js
@@ -47,6 +47,4 @@ export default function App () {
      </NavigationContainer>
    );
 
-
-
-}
+      }
--- screens/EventsScreen.js
+++ screens/EventsScreen.js
@@ -8,20 +8,38 @@ function EventsHomeScreen ({navigation}){
            <Text>Events First Page!</Text>
        
        <Button
-       onPress={() => NavigationPreloadManager.navigate("EventsSecond")}
+       onPress={() => navigation.navigate("EventsSecond")}
        title="Second screen"></Button>
        </View>
    );
 
 }
 
-function EventsSecondScreen({navigation}){
-    return(
-        <View style={{ flex: 1, justifyContent: "center", alignItems:"center"}}>
-            <Text>Events Second Page!</Text>
-        </View>
+function EventsSecondScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Events Second Page!</Text>
+        <Button
+          onPress={() => navigation.navigate("EventsThird")}
+          title="Third Screen"
+        ></Button>
+      </View>
     );
-}
+   }
+   
+
+function EventsThirdScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Events Third Page!</Text>
+        <Button
+          onPress={() => navigation.popToTop()}
+          title="Back to top"
+        ></Button>
+      </View>
+    );
+   }
+   
 
 const Stack = createStackNavigator();
 
@@ -31,6 +49,7 @@ export default function EventsStack() {
         <Stack.Navigator>
             <Stack.Screen name="EventsHome" component={EventsHomeScreen} />
             <Stack.Screen name="EventsSecond" component={EventsSecondScreen} />
+            <Stack.Screen name="EventsThird" component={EventsThirdScreen} />
         </Stack.Navigator>
     );
 }
