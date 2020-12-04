07a61fd03655896eafb6ce753793e687f216bf7d
Guan Xian <gxleow@gmail.com>
2020-12-04 10:31:18 +0800

    First change

---
+++ screens/EventsScreen.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {Text,View,Button} from "react-native";
+import { createStackNavigator} from "@react-navigation/stack";
+
+function EventsHomeScreen ({navigation}){
+   return (
+       <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+           <Text>Events First Page!</Text>
+       
+       <Button
+       onPress={() => NavigationPreloadManager.navigate("EventsSecond")}
+       title="Second screen"></Button>
+       </View>
+   );
+
+}
+
+function EventsSecondScreen({navigation}){
+    return(
+        <View style={{ flex: 1, justifyContent: "center", alignItems:"center"}}>
+            <Text>Events Second Page!</Text>
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
+
+export default function EventsStack() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="EventsHome" component={EventsHomeScreen} />
+            <Stack.Screen name="EventsSecond" component={EventsSecondScreen} />
+        </Stack.Navigator>
+    );
+}
